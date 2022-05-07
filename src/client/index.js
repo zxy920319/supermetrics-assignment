3f46eb763c5770c5e8fe8224eeab954dee70811b
zxy920319 <zxy920319@hotmail.com>
2022-05-07 21:14:45 +0100

    refersh token after 1 hour

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,10 @@
 import axios from "axios"
+import { useAuthStore } from "@/stores/auth"
 
 const client = axios.create({ baseURL: import.meta.env.VITE_APP_API_URL })
 
 const get = async (url, config) => {
+  await checkToken()
   try {
     const response = await client.get(url, config)
     return response.data
@@ -11,7 +13,8 @@ const get = async (url, config) => {
   }
 }
 
-const post = async (url, data, config) => {
+const post = async (url, data, config, isGetToken = false) => {
+  if (!isGetToken) await checkToken()
   try {
     const response = await client.post(url, data, config)
     return response.data
@@ -20,13 +23,26 @@ const post = async (url, data, config) => {
   }
 }
 
+const checkToken = async () => {
+  const store = useAuthStore()
+  const current = new Date().valueOf()
+  if (current - store.timeStamp > 1000 * 60 * 60) {
+    await store.refreshToken()
+  }
+}
+
 const getToken = async () => {
   try {
-    const { data } = await post("/register", {
-      client_id: import.meta.env.VITE_CLIENT_ID,
-      email: import.meta.env.VITE_EMAIL,
-      name: import.meta.env.VITE_NAME,
-    })
+    const { data } = await post(
+      "/register",
+      {
+        client_id: import.meta.env.VITE_CLIENT_ID,
+        email: import.meta.env.VITE_EMAIL,
+        name: import.meta.env.VITE_NAME,
+      },
+      {},
+      true
+    )
     const { sl_token } = data
     return sl_token
   } catch (error) {
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -3,10 +3,11 @@ import { getToken } from "@/client"
 
 export const useAuthStore = defineStore("auth", {
   state: () => {
-    return { token: null }
+    return { token: null, timeStamp: null }
   },
   actions: {
     async refreshToken() {
+      this.timeStamp = new Date().valueOf()
       this.token = await getToken()
     },
   },

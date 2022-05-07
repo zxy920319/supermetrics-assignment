b55f56e415e3fd3350d949bd14830d97dd688223
zxy920319 <zxy920319@hotmail.com>
2022-05-07 17:43:48 +0100

    install ant design

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import eslintPlugin from "vite-plugin-eslint"
 import path from "path"
+import Components from "unplugin-vue-components/vite"
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
+
+Components({
+  resolvers: [AntDesignVueResolver()],
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
